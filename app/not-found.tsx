67947f9d47b1c2a4b8974ac1e6bc0cc37b9838aa
unaivan22 <unaivan22@gmail.com>
2024-08-29 16:00:09 +0700

    commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 items-start">
-      <div>
-        <h2 className="text-5xl font-bold">404</h2>
-        <p className="text-muted-foreground">Page not found</p>
-      </div>
+    <div className="grid h-screen place-items-center">
+      <div className="flex flex-col gap-4 items-center">
+        <div className="flex flex-col items-center gap-2 justify-center text-center">
+          <h2 className="text-[4rem] font-bold">404</h2>
+          <p className="text-muted-foreground">Page not found</p>
+        </div>
 
-      <Link href="/" className={buttonVariants({})}>
-        Back to homepage
-      </Link>
+        <Link href="/" className={buttonVariants({})}>
+          Back to homepage
+        </Link>
+      </div>
     </div>
   );
 }
